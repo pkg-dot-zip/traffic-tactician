526318b038ebb7bf45d224644b25663d3f4339c6
Mosu <m.azaimi@student.avans.nl>
2024-05-07 13:46:06 +0200

    add 3d project template main.cpp

---
+++ TrafficTactician/main.cpp
@@ -0,0 +1,70 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include "tigl.h"
+#include <glm/gtc/matrix_transform.hpp>
+using tigl::Vertex;
+
+
+
+#pragma comment(lib, "glfw3.lib")
+#pragma comment(lib, "glew32s.lib")
+#pragma comment(lib, "opengl32.lib")
+
+GLFWwindow* window;
+
+void init();
+void update();
+void draw();
+
+int main(void)
+{
+    if (!glfwInit())
+        throw "Could not initialize glwf";
+    window = glfwCreateWindow(1400, 800, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        throw "Could not initialize glwf";
+    }
+    glfwMakeContextCurrent(window);
+
+    tigl::init();
+
+    init();
+
+	while (!glfwWindowShouldClose(window))
+	{
+		update();
+		draw();
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+
+
+    return 0;
+}
+
+
+void init()
+{
+    glfwSetKeyCallback(window, [](GLFWwindow* window, int key, int scancode, int action, int mods)
+    {
+        if (key == GLFW_KEY_ESCAPE)
+            glfwSetWindowShouldClose(window, true);
+    });
+
+}
+
+
+void update()
+{
+
+}
+
+void draw()
+{
+    glClearColor(0.3f, 0.4f, 0.6f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+}
