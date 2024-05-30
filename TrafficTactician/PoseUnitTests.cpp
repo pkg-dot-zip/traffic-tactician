c56b06d7f10be88f4b9b40586d1ae38fbe4c952d
pkg-dot-zip <simon@pkg-dot-zip.com>
2024-05-30 13:14:41 +0200

    Added more test cases

--- TrafficTactician/PoseUnitTests.cpp
+++ TrafficTactician/PoseUnitTests.cpp
@@ -14,6 +14,7 @@
 #include "utest.h"
 #include <opencv2/dnn.hpp>
 
+#pragma region helpers
 extern std::map<std::string, std::vector<KeyPoint>> poseEstimationKeyPoints;
 
 std::map<std::string, std::vector<KeyPoint>>& getPoints(std::string_view filePath)
@@ -24,14 +25,16 @@ std::map<std::string, std::vector<KeyPoint>>& getPoints(std::string_view filePat
 	cv::dnn::Net inputNet;
 	cv::Mat outputFrame;
 
-	loadDnnModel(inputNet);
+	if (inputNet.empty()) loadDnnModel(inputNet);
 
 	poseEstimationKeyPoints = getPoseEstimationKeyPointsMap(
 		input1, outputFrame, inputNet);
 
 	return poseEstimationKeyPoints;
 }
+#pragma endregion helpers
 
+#pragma region returnsMoveLeft
 UTEST(getPose, returnsMoveLeft1)
 {
 	ASSERT_EQ(getPose(getPoints("./images/unittesting/move_left1.jpg")), POSE_MOVE_LEFT);
@@ -46,7 +49,26 @@ UTEST(getPose, returnsMoveLeft3)
 {
 	ASSERT_EQ(getPose(getPoints("./images/unittesting/move_left3.jpg")), POSE_MOVE_LEFT);
 }
+#pragma endregion returnsMoveLeft
 
+#pragma region NOTreturnsMoveLeft
+UTEST(getPose, NOTreturnsMoveLeft1)
+{
+	ASSERT_NE(getPose(getPoints("./images/unittesting/not_move_left1.jpg")), POSE_MOVE_LEFT);
+}
+
+UTEST(getPose, NOTreturnsMoveLeft2)
+{
+	ASSERT_NE(getPose(getPoints("./images/unittesting/not_move_left2.jpg")), POSE_MOVE_LEFT);
+}
+
+UTEST(getPose, NOTreturnsMoveLeft3)
+{
+	ASSERT_NE(getPose(getPoints("./images/unittesting/not_move_left3.jpg")), POSE_MOVE_LEFT);
+}
+#pragma endregion NOTreturnsMoveLeft
+
+#pragma region returnsMoveRight
 UTEST(getPose, returnsMoveRight1)
 {
 	ASSERT_EQ(getPose(getPoints("./images/unittesting/move_right1.jpg")), POSE_MOVE_RIGHT);
@@ -61,5 +83,18 @@ UTEST(getPose, returnsMoveRight3)
 {
 	ASSERT_EQ(getPose(getPoints("./images/unittesting/move_right3.jpg")), POSE_MOVE_RIGHT);
 }
+#pragma endregion returnsMoveRight
+
+#pragma region NOTreturnsMoveRight
+UTEST(getPose, NOTreturnsMoveRight1)
+{
+	ASSERT_NE(getPose(getPoints("./images/unittesting/not_move_right1.jpg")), POSE_MOVE_RIGHT);
+}
+
+UTEST(getPose, NOTreturnsMoveRight2)
+{
+	ASSERT_NE(getPose(getPoints("./images/unittesting/not_move_right2.jpg")), POSE_MOVE_RIGHT);
+}
+#pragma endregion NOTreturnsMoveRight
 
 #endif
