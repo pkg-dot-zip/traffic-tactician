14fdde47991560288276855cb9a736e077feaf30
Moustapha Azaïmi <m.azaimi@student.avans.nl>
2024-06-13 21:42:24 +0200

    moved generator initialization out of getRandomNumber()

--- TrafficTactician/RandomGenerator.cpp
+++ TrafficTactician/RandomGenerator.cpp
@@ -3,11 +3,11 @@
 namespace RandomGenerator
 {
 	std::random_device rd_;
-	std::mt19937 generator_;
+	std::mt19937 generator_ = std::mt19937(rd_());
+
 	std::uniform_int_distribution<> dis_;
 
 	int getRandomNumber(int min, int max) {
-		generator_ = std::mt19937(rd_());
 		dis_ = std::uniform_int_distribution<>(min, max);
 		return dis_(generator_);
 	}
